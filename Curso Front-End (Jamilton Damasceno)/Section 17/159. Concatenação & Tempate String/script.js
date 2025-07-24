7efbd8c9188b99200054fd6c8a9d503926f9a467
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-07-24 10:58:53 -0300

    Class 159

---
+++ Curso Front-End (Jamilton Damasceno)/Section 17/159. Concatenação & Tempate String/script.js
@@ -0,0 +1,6 @@
+let nome = 'Guilherme'
+let carrinho = 2
+
+console.log(nome , 'possui' , carrinho , 'produtos no carrinho!') //Concatenação
+
+console.log(`${nome} possui ${carrinho} produtos no carrinho!`) //Template String
