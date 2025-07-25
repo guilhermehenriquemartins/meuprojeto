b07752e7355d54ac5631b7671d7ccc810981246b
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-07-25 08:49:55 -0300

    Class 161

---
+++ Curso Front-End (Jamilton Damasceno)/Section 17/161. Loops - do, while e for/script.js
@@ -0,0 +1,15 @@
+let postagens = [
+  "Hoje passeando pela av paulista",
+  "Passeando em Campos do Jordão",
+  "Hoje fiz um curso de criação de sites",
+  "Na casa da mamãe, aproveitando o dia!"
+]
+
+for (indice in postagens) {
+  console.log(postagens[indice])
+}
+
+
+for(numero = 0; numero < postagens.length; numero++) {
+  console.log(postagens[numero])
+}
