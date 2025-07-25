b6d4d3df4952dfee856f666347e01c7a7ab64899
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-07-25 11:24:38 -0300

    Class 162. [PROJETO] Lista Nomes

---
+++ Curso Front-End (Jamilton Damasceno)/Section 17/162. [PROJETO] Lista Nomes/js/script.js
@@ -0,0 +1,41 @@
+let form = window.document.getElementsByTagName('form')[0]
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+})
+
+let texto = window.document.getElementById('nome-usuario')
+
+const nomes = [
+  'guilherme',
+  'josé',
+  'letícia',
+  'maria',
+  'carlos',
+  'pedro',
+  'marcela',
+  'carla',
+  'joão'
+]
+
+function carregou() {
+  let itensLista = ''
+
+  for (indice in nomes) {
+    let nome = nomes[indice]
+    itensLista += `<li class="item">${nome}</li>`
+  }
+
+  document.getElementsByTagName('ul')[0].innerHTML = itensLista
+}
+
+function pesquisou() {
+  itensLista = ''
+  for (indice in nomes) {
+    if (texto.value.toLowerCase() == nomes[indice]) {
+      let nome = nomes[indice]
+      itensLista += `<li class="item">${nome}</li>`
+    }
+  }
+
+  document.getElementsByTagName('ul')[0].innerHTML = itensLista
+}
