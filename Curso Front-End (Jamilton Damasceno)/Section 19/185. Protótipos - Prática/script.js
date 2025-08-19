08da4f29432df304579bb9c17fa513272591e529
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-08-19 15:28:49 -0300

    Class 185

---
+++ Curso Front-End (Jamilton Damasceno)/Section 19/185. Protótipos - Prática/script.js
@@ -0,0 +1,10 @@
+//Descendentes de Object
+
+//Todo objeto que criamos em JavaScript são descendentes de Object
+Object.prototype
+
+const obj = {
+
+}
+
+console.log(obj.__proto__ == Object.prototype)
