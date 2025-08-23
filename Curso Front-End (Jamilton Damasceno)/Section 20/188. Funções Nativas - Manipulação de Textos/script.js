2895ba630d3730a92ef000d845611cd239374541
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-08-23 11:13:56 -0300

    Class 188

---
+++ Curso Front-End (Jamilton Damasceno)/Section 20/188. Funções Nativas - Manipulação de Textos/script.js
@@ -0,0 +1,35 @@
+//let nome = "                Guilherme Henrique Fiorio Martins      "
+
+/*
+console.log(nome.length)
+*/
+
+/*
+console.log(nome.charAt(6))
+*/
+
+/*
+console.log(nome.replace("Henrique", "Martins"))
+*/
+
+/*
+console.log(nome.substr(19, 6))
+*/
+
+/*
+let n = nome.split(" ")
+console.log(n[3])
+*/
+
+/*
+console.log(nome.trim())
+*/
+
+/*
+let nome1 = "Guilherme"
+let nome2 = "Henrique"
+let nome3 = "Fiorio"
+let nome4 = "Martins"
+
+console.log(nome1.concat(" ", nome2, " ", nome3, " ", nome4))
+*/
