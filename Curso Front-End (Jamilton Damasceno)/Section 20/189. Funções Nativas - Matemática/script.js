2fe1ee9fe0446556cbeb5e63b086e54cae52b09f
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-08-25 11:08:12 -0300

    Class 189

---
+++ Curso Front-End (Jamilton Damasceno)/Section 20/189. Funções Nativas - Matemática/script.js
@@ -0,0 +1,11 @@
+let numero = 280.49
+
+//console.log(Math.PI.toFixed(2))
+//console.log(Math.ceil(numero))
+//console.log(Math.floor(numero))
+//console.log(Math.round(numero))
+//console.log(Math.round(Math.random() * 10))
+//console.log(Math.min(10, 3))
+//console.log(Math.max(10, 5))
+//console.log(Math.pow(2, 10))
+//console.log(Math.sqrt(25))
