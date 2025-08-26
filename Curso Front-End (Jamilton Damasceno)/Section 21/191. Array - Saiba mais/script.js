a6b1ebb8ff4b9dfcde571e43fb65d662819bac6e
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-08-26 14:43:16 -0300

    Class 191

---
+++ Curso Front-End (Jamilton Damasceno)/Section 21/191. Array - Saiba mais/script.js
@@ -0,0 +1,13 @@
+/*const nomes = ["Guilherme", "Maria"]
+const frutas = new Array("Morango", "Banana")
+
+console.log(frutas[0])
+*/
+
+const nomes = ["Guilherme","Maria", "Ana"]
+nomes.push("Pedro")
+//delete nomes[2]
+//nomes[0] = "Henrique"
+nomes.sort()
+
+console.log(nomes)
