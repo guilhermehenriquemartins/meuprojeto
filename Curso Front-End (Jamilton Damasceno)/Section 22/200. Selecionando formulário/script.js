603889c8f9c8ed3bf2180268ff8481b1649446bd
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-09-08 16:30:30 -0300

    Class 200

---
+++ Curso Front-End (Jamilton Damasceno)/Section 22/200. Selecionando formulário/script.js
@@ -0,0 +1,5 @@
+function Carregou() {
+  document.cadastro.nome.value = "Guilherme"
+  document.cadastro.email.value = "guilhermehenriquefiorio@outlook.com"
+  document.cadastro.sexo.value = "masculino"
+}
