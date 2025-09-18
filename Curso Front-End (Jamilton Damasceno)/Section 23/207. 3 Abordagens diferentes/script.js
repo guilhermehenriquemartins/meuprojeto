1a2f8e61d9ee56ba0823166a2da186a15a0341bc
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2025-09-18 11:25:28 -0300

    Class 207

---
+++ Curso Front-End (Jamilton Damasceno)/Section 23/207. 3 Abordagens diferentes/script.js
@@ -0,0 +1,6 @@
+/*let botao = document.getElementsByTagName('button')[0]*/
+let botao = document.querySelector('[botao-acao]')
+
+botao.addEventListener('click', function(e) {
+  alert('Clicou!')
+})
